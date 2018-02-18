513d047980098e84f785c800c720b623ca2881c7
Rehan Jaffer <ray@thelondonvandal.com>
2018-02-18 18:34:47 +0000

    all that clunky code can be replaced with use of the getline() function from stdio

--- main.c
+++ main.c
@@ -52,37 +52,8 @@ int execute(char **args) {
 
 
 char *read_line(void) {
-  int bufsize = RUSH_RL_BUFSIZE;
-  int position = 0;
-  char *buffer = malloc(sizeof(char) * bufsize);
-  int c;
-
-  if (!buffer) {
-    fprintf(stderr, "rush: malloc error");
-    exit(EXIT_FAILURE);
-  }
-
-  while (1) {
-    c = getchar();
-
-    if (c == EOF || c == '\n') {
-      buffer[position] = '\0';
-      return buffer;
-    } else {
-      buffer[position] = c;
-    }
-
-    position++;
-
-    if (position >= bufsize) {
-      bufsize += RUSH_RL_BUFSIZE;
-      buffer = realloc(buffer, bufsize);
-      if (!buffer) {
-        fprintf(stderr, "rush: allocation error\n");
-        exit(EXIT_FAILURE);
-      }
-    }
-
-  }
-
+  char *line = NULL;
+  ssize_t bufsize = 0;
+  getline(&line, &bufsize, stdin);
+  return line;
 }
