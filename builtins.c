fc38a2040923b0d5b374f86075842cb95954bb58
Rehan Jaffer <ray@thelondonvandal.com>
2018-02-19 16:15:54 +0000

    should be returning 1 to avoid exiting, added actual chdir call

--- builtins.c
+++ builtins.c
@@ -4,13 +4,17 @@ int rush_cd(char **args) {
   if (args[1] == NULL) {
     fprintf(stderr, "rush: expected argument to 'cd'\n");
   } else {
-    perror("rush");
+    if (chdir(args[1]) != 0) {
+      perror("rush");
+    }
   }
+  return 1;
 }
 
 int rush_help(char **args) {
   int i;
   printf("AHHHH HELP ME\r\n");
+  return 1;
 }
 
 int rush_exit(char **args) {
