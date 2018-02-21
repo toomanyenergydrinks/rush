2e668ea71afe3eb95dba04f6b2b9021c991e16ee
Rehan Jaffer <ray@thelondonvandal.com>
2018-02-21 11:07:54 +0000

    removed history from builtins, moved into its own file

--- builtins.c
+++ builtins.c
@@ -1,19 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 
-extern char history[10][255];
-extern int history_ptr;
-
-int rush_history(char **args) {
-  int x;
-
-  for (x=0;x<HISTORY_SIZE;x++) {
-    printf("%s\n", history[(history_ptr+x) % HISTORY_SIZE]);
-  }
-
-  return 1;
-}
-
 int rush_cd(char **args) {
   if (args[1] == NULL) {
     fprintf(stderr, "rush: expected argument to 'cd'\n");
